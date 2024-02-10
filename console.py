9d478546c2bf2d003ccb96a74d5565f8c8f11f90
Terry (shenkezs) <shenkez23@gmail.com>
2024-02-10 05:32:29 -0800

    console now

---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Command interpreter module"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program """
+        print("Am quiting this program")
+        return True
+
+    def do_EOF(self, arg):
+        """This func help exit the program. """
+        print("")
+        return True
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,8 @@
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+'''__init__ method: This code is meant to initialize the FileStorage
+class from the models.engine.file_storage module and reload data from a JSON file'''
 
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
 
